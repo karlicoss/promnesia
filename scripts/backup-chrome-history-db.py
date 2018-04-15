4631b4eeabf1bb10f81905ec81ac239104d81cbe
Dima Gerasimov <karlicoss@gmail.com>
2018-04-15 17:55:01 +0100

    Basic test for chrome history extractor

--- scripts/backup-chrome-history-db.py
+++ scripts/backup-chrome-history-db.py
@@ -16,15 +16,9 @@ def atomic_copy(src: str, dest: str):
         res = shutil.copy(src, dest)
         differs = not filecmp.cmp(src, res)
 
-def main():
-    import argparse
-    parser = argparse.ArgumentParser(description="Backup tool for chrome history db")
-    parser.add_argument('backup-prefix', type=str, help="Base folder for chrome DB backups. Should generally be same as CHROME_HISTORY_DB_DIR in config.py")
-    args = parser.parse_args()
-    PREFIX = getattr(args, 'backup-prefix')
-
-    TODAY = datetime.now().strftime("%Y%m%d")
-    BPATH = f"{PREFIX}/{TODAY}/"
+def backup_to(prefix: str):
+    today = datetime.now().strftime("%Y%m%d")
+    BPATH = f"{prefix}/{today}/"
 
     os.makedirs(BPATH, exist_ok=True)
 
@@ -36,6 +30,15 @@ def main():
     # if your chrome is open, database would normally be locked, so you can't just make a snapshot
     # so we'll just copy it till it converge. bit paranoid, but should work
     atomic_copy(DB, BPATH)
+
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser(description="Backup tool for chrome history db")
+    parser.add_argument('backup-prefix', type=str, help="Base folder for chrome DB backups. Should generally be same as CHROME_HISTORY_DB_DIR in config.py")
+    args = parser.parse_args()
+    PREFIX = getattr(args, 'backup-prefix')
+    backup_to(prefix)
 
 if __name__ == '__main__':
     main()
--- test.py
+++ test.py
@@ -6,3 +6,15 @@ def test_takeout():
     histories = takeout_gen.get_takeout_histories(test_takeout_dir)
     [hist] = histories
     pass
+
+def test_chrome():
+    from tempfile import TemporaryDirectory
+    import wereyouhere.generator.chrome as chrome_gen
+    import imp
+
+    with TemporaryDirectory() as tdir:
+        backup_db = imp.load_source('hdb', 'scripts/backup-chrome-history-db.py')
+        backup_db.backup_to(tdir)
+
+        [hist] = list(chrome_gen.iter_chrome_histories(tdir))
+        assert len(hist) > 10 # kinda random sanity check
