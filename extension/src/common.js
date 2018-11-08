3bb6a33e61e74efe2de026d37e48ac63cb846189
Dima Gerasimov <karlicoss@gmail.com>
2018-11-08 20:45:43 +0000

    [extension] initial support for Flow

--- extension/src/common.js
+++ extension/src/common.js
@@ -1,5 +1,14 @@
+/* @flow */
+
+export class Visit {
+    // TODO parse it
+}
+
 export class Visits {
-    constructor(visits, contexts) {
+    visits: Array<any>;
+    contexts: Array<any>;
+
+    constructor(visits: Array<any>, contexts: Array<any>) {
         this.visits = visits;
         this.contexts = contexts;
     }
