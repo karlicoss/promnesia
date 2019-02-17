dc723823c882c91e702b83cad858b005397dc4eb
Dima Gerasimov <karlicoss@gmail.com>
2019-02-17 22:34:30 +0000

    escaping grep

--- wereyouhere/generator/plaintext.py
+++ wereyouhere/generator/plaintext.py
@@ -4,26 +4,28 @@ from pathlib import Path
 import os.path
 import os
 
+from shlex import quote
+
 # https://linux-and-mac-hacks.blogspot.co.uk/2013/04/use-grep-and-regular-expressions-to.html
 _URL_REGEX = r'\b(https?|ftp|file)://[-A-Za-z0-9+&@#/%?=~_|!:,.;]*[-A-Za-z0-9+&@#/%=~_|]'
 
 # -n to output line numbers so we could restore context
 # -I to ignore binaries
-_GREP_CMD = r"""grep --color=never -Eo -I {grep_args} --exclude="*.html~" --exclude="*.html" --exclude-dir=".git" '{regex}' {path}"""
+_GREP_CMD = r"""grep --color=never -Eo -I {grep_args} --exclude="*.html~" --exclude="*.html" --exclude-dir=".git" '{regex}' {path} || true"""
 
 
 def _extract_from_dir(path: str) -> str:
     return _GREP_CMD.format(
         grep_args="-r -n",
         regex=_URL_REGEX,
-        path=path,
+        path=path, # TODO quote here too?
     )
 
 def _extract_from_file(path: str) -> str:
     return _GREP_CMD.format(
         grep_args="-n",
         regex=_URL_REGEX,
-        path=f"'{path}' /dev/null", # dev null to trick into displaying filename
+        path=f"{quote(path)} /dev/null", # dev null to trick into displaying filename
     )
 
 
