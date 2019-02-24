50494725c781985c1fabf7069b076bd13bf8e896
Dima Gerasimov <karlicoss@gmail.com>
2019-02-24 11:21:12 +0000

    add support for intermediate stats

--- wereyouhere/__main__.py
+++ wereyouhere/__main__.py
@@ -4,8 +4,9 @@ import logging
 import json
 import inspect
 from sys import exit
-from typing import List, Tuple
+from typing import List, Tuple, Optional
 from pathlib import Path
+from datetime import datetime
 
 def import_config(config_file: str):
     import os, sys, importlib
@@ -22,9 +23,9 @@ from .common import Entry, Visit, History, Filter, make_filter, get_logger, get_
 from .render import render
 
 # TODO smart is misleading... perhaps, get rid of it?
-from wereyouhere.generator.smart import previsits_to_history
+from wereyouhere.generator.smart import previsits_to_history, Wrapper
 
-def run(config_file: str):
+def run(config_file: str, intermediate: Optional[str]):
     logger = get_logger()
 
     config = import_config(config_file)
@@ -36,6 +37,9 @@ def run(config_file: str):
     if not output_dir.exists():
         raise ValueError("Expecting OUTPUT_DIR to be set to a correct path!")
 
+    intm = output_dir.joinpath('intermediate') if intermediate is None else Path(intermediate)
+    intm.mkdir(exist_ok=True)
+
     filters = [make_filter(f) for f in config.FILTERS]
     for f in filters:
         History.add_filter(f) # meh..
@@ -45,15 +49,44 @@ def run(config_file: str):
     had_errors = False
 
     for extractor in extractors:
+        ex = extractor
+        # TODO isinstance wrapper?
+        # TODO make more defensive?
+        einfo: str
+        if isinstance(ex, Wrapper):
+            einfo = f'{ex.ff.__module__}:{ex.ff.__name__} {ex.args} {ex.kwargs}'
+        else:
+            einfo = f'{ex.__module__}:{ex.__name__}'
+
         hist, errors = previsits_to_history(extractor)
         if len(errors) > 0:
             had_errors = True
-        all_histories.append(hist)
+        all_histories.append((einfo, hist))
+
+    # this is intermediate, to make future raw data comparisons easier...
+    intermediates = []
+    for e, h in all_histories:
+        cur = []
+        # TODO what do we do in case of parity?
+        for entry in sorted(h.urls.values(), key=lambda e: e.url):
+            url = entry.url
+            visits = list(sorted(entry.visits, key=lambda v: (v.dt.isoformat(), v.context or ''))) # isoformat just to get away with comparing aware/unaware...
+            cur.append({
+                'url': url,
+                'visits': [{
+                    'dt': v.dt.isoformat(), # hopefully that's ok; python 3.7 is capable of deserializing it, but I might have to backport it
+                    'tag': v.tag,
+                    'context': v.context,
+                } for v in visits],
+            })
+        intermediates.append((e, cur))
+    with intm.joinpath(datetime.utcnow().strftime('%Y%m%d%H%M%S.json')).open('w') as fo:
+        json.dump(intermediates, fo, ensure_ascii=False, sort_keys=True, indent=1)
+
+    history = merge_histories([h for _, h in all_histories])
+    j = render(history, fallback_timezone=fallback_tz)
 
     urls_json = output_dir.joinpath('linksdb.json')
-
-    history = merge_histories(all_histories)
-    j = render(history, fallback_timezone=fallback_tz)
     with urls_json.open('w') as fo:
         json.dump(j, fo, indent=1, ensure_ascii=False)
 
@@ -67,10 +100,14 @@ def main():
 
     p = argparse.ArgumentParser()
     p.add_argument('--config', default='config.py')
+    p.add_argument('--intermediate', required=False)
+    # TODO maybe, it's better for server to compute intermediate represetnation?
+    # the only downside is storage. dunno.
+    # worst case -- could use database?
     args = p.parse_args()
 
     try:
-        run(config_file=args.config)
+        run(config_file=args.config, intermediate=args.intermediate)
     except:
         tdir = get_tmpdir()
         tdir.cleanup()
