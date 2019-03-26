31ad5ae3c3b740797dddd70d2d1d4f773af5d166
Dima Gerasimov <karlicoss@gmail.com>
2019-03-26 23:10:28 +0000

    better comparison

--- scripts/compare-intermediate.py
+++ scripts/compare-intermediate.py
@@ -2,9 +2,12 @@
 from datetime import datetime
 import argparse
 from pathlib import Path
+import sys
 import json
 import logging
 from typing import Dict, List, Any, NamedTuple, Optional, Set
+
+from hashlib import sha1
 
 from kython.klogging import setup_logzero
 
@@ -29,6 +32,12 @@ class Visit(NamedTuple):
     source: Source
     context: Context
     tag: Tag
+
+
+    # special id for easier excluding..
+    @property
+    def exid(self):
+        return sha1('!'.join([self.url, self.dt, self.source, self.context, self.tag]).encode('utf8')).hexdigest()
 
 
 def ddiff(a, b):
@@ -65,9 +74,17 @@ def eliminate_by(sa, sb, key):
     return onlya, common, onlyb
 
 
-def compare(before: Set[Visit], after: Set[Visit], before_fdt=None):
+def compare(before: Set[Visit], after: Set[Visit], between: str) -> List[Visit]:
+    errors: List[Visit] = []
+
+    def reg_error(b):
+        errors.append(b)
+        logger.error('between %s missing %s', between, b)
+        logger.error('ignore line: "%s", # %s %s', b.exid, b.url, b.tag)
+
+
     logger = get_logger()
-    # optimisation: elimiante common
+    # optimisation: eliminate common
 
     eliminations = [
         ('identity'               , lambda x: x),
@@ -82,12 +99,15 @@ def compare(before: Set[Visit], after: Set[Visit], before_fdt=None):
         logger.info('common: %d, before: %d, after: %d', len(common), len(before), len(after))
 
     logger.info('removing explicitly ignored items')
-    before = {b for b in before if not ignore(b, fdt=before_fdt)}
+    before = {b for b in before if not ignore(b, between=between)}
     logger.info('before: %d', len(before))
 
 
     for b in before:
-        logger.error('missing %s', b)
+        reg_error(b)
+
+    return errors
+
 
 def collect(jj):
     logger = get_logger()
@@ -106,7 +126,9 @@ def collect(jj):
                 if vs in visits:
                     # TODO FIXME multiset??
                     # TODO debug level? not sure if should show them at all
-                    logger.warning('duplicate visit %s', vs)
+                    # TODO FIXME shit. ok, duplicates are coming from different takeouts apparently. enable back once I merge properly...
+                    # logger.warning('duplicate visit %s', vs)
+                    pass
                 #     import ipdb; ipdb.set_trace() 
                 visits.add(vs)
     return visits
@@ -118,23 +140,37 @@ def main():
     p = argparse.ArgumentParser()
     # TODO better name?
     p.add_argument('--intermediate-dir', type=Path, required=True)
+    p.add_argument('--all', action='store_true')
     args = p.parse_args()
     # TODO perhaps get rid of linksdb completely? The server could merge them by itself
     int_dir = args.intermediate_dir
     assert int_dir.exists()
 
     jsons = list(sorted(int_dir.glob('*.json')))
+    if not args.all:
+        # only compare last
+        jsons = jsons[-2:]
+
     assert len(jsons) > 0
 
+    all_errors = []
+
     last = None
-    last_dt = None
+    last_dts = None
     for f in jsons:
         logger.info('processing %r', f)
+        this_dts = f.stem
         vis = collect(json.loads(f.read_text()))
         if last is not None:
-            compare(last, vis, before_fdt=last_dt)
+            between = f'{last_dts}:{this_dts}'
+            errs = compare(last, vis, between=between)
+            all_errors.extend(errs)
         last = vis
-        last_dt = datetime.strptime(f.stem, '%Y%m%d%H%M%S')
+        last_dts = this_dts
+
+    if len(all_errors) > 0:
+        sys.exit(1)
 
 if __name__ == '__main__':
     main()
+
