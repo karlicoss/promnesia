a5181b8fa3d7fea1d7d52893591ac8f3fb64e30c
Dima Gerasimov <karlicoss@gmail.com>
2019-04-21 10:43:38 +0100

    proper logging

--- server/wereyouhere_server.py
+++ server/wereyouhere_server.py
@@ -6,14 +6,26 @@ import os
 import sys
 from datetime import timedelta, datetime
 from pathlib import Path
+import functools
+import logging
+
+from kython import setup_logzero
 
 import hug # type: ignore
 import hug.types as T # type: ignore
 
 
-def log(*things):
-    # TODO proper logging
-    print(*things, file=sys.stderr, flush=True)
+# TODO cachy thing from kython??
+
+
+# meh. need this since I don't have hooks in hug to initialize logging properly..
+logging_initialized = False
+def get_logger():
+    logger = logging.getLogger('wereyouhere')
+    global logging_initialized
+    if not logging_initialized:
+        setup_logzero(logger, level=logging.DEBUG)
+    return logger
 
 
 # TODO how to have a worker in parallel??
@@ -30,12 +42,13 @@ class State:
         self.last = None
 
     def refresh(self):
+        logger = get_logger()
         now = datetime.now()
         if self.last is not None and (now - self.last) < DELTA:
             return
 
         # TODO use that?? https://github.com/timothycrosley/hug/blob/develop/tests/test_async.py
-        log("Reloading the map")
+        logger.info("Reloading the map")
         with self.links.open('r') as fo:
             self.vmap = json.load(fo)
         self.last = now
@@ -70,10 +83,11 @@ from normalise import normalise_url
 def visits(
         url: T.text,
 ):
+    logger = get_logger()
     url = normalise_url(url)
     vmap = get_state().get_map()
     res = vmap.get(url, None)
-    log(res)
+    logger.debug(res)
     return res
 
 @hug.local()
@@ -81,9 +95,10 @@ def visits(
 def visited(
         urls, # TODO type
 ):
+    logger = get_logger()
     vmap = get_state().get_map()
     nurls = list(map(normalise_url, urls))
-    log(nurls)
+    logger.debug(nurls)
     return [
         u in vmap for u in nurls
     ]
@@ -108,6 +123,7 @@ def setup_parser(p):
     p.add_argument('--links', type=str, default='/L/data/wereyouhere/linksdb.json', help='Path to links database')
 
 def main():
+    setup_logzero(get_logger(), level=logging.DEBUG)
     p = argparse.ArgumentParser('wereyouhere server', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     setup_parser(p)
     args = p.parse_args()
