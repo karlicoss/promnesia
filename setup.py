f00ab00088eac0c769974168f96d49d33e68b6de
Dima Gerasimov <karlicoss@gmail.com>
2019-08-02 09:05:20 +0100

    initial tox + setup file

---
+++ setup.py
@@ -0,0 +1,21 @@
+import sys
+
+from pkg_resources import require, VersionConflict
+from setuptools import setup
+
+try:
+    require('setuptools>=38.3')
+except VersionConflict:
+    print("Error: version of setuptools is too old (<38.3)!")
+    sys.exit(1)
+
+
+if __name__ == "__main__":
+    setup(
+        use_pyscaffold=True,
+        package_data={
+            'wereyouhere': [
+                'py.typed',
+            ],
+        },
+    )
