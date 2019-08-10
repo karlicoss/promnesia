14ba396f55801ba7397ff71ec35181c8c2071ee5
Dima Gerasimov <karlicoss@gmail.com>
2019-08-10 15:54:44 +0100

    common wrapper for async chrome methods

--- extension/src/async_chrome.js
+++ extension/src/async_chrome.js
@@ -1,29 +1,35 @@
 /* @flow */
 
-// $FlowFixMe
-export function chromeTabsExecuteScriptAsync(...args) {
-    // $FlowFixMe
+function wrap(fn, ...args: Array<any>): Promise<any> {
     return new Promise(cb => {
-        chrome.tabs.executeScript(...args, cb);
-        const err = chrome.runtime.lastError;
-        if (err) {
-            throw err;
-        }
+        const cbb = (...xxx) => {
+            const err = chrome.runtime.lastError;
+            if (err) {
+                throw err;
+            }
+            cb(...xxx);
+        };
+        fn(...args, cbb);
     });
 }
 
-// $FlowFixMe
-export function chromeTabsInsertCSS(...args) {
-    // $FlowFixMe
-    return new Promise(cb => chrome.tabs.insertCSS(...args, cb));
+export function chromeTabsExecuteScriptAsync(tabId: number, opts: any) {
+    return wrap(chrome.tabs.executeScript, tabId, opts);
+}
+
+export function chromeTabsInsertCSS(tabId: number, opts: any) {
+    return wrap(chrome.tabs.insertCSS, tabId, opts);
 }
 
 export function chromeTabsQueryAsync(opts: any): Promise<Array<chrome$Tab>> {
-    return new Promise(cb => chrome.tabs.query(opts, cb));
+    return wrap(chrome.tabs.query, opts);
 }
 
 
 export function chromeRuntimeGetPlatformInfo(): Promise<chrome$PlatformInfo> {
-    return new Promise(cb => chrome.runtime.getPlatformInfo(cb));
+    return wrap(chrome.runtime.getPlatformInfo);
 }
 
+export function chromeRuntimeSendMessage(msg: any) {
+    return wrap(chrome.runtime.sendMessage, msg);
+}
