138c65d1750adf4891ffcbfca143c3d80b8e441d
Dima Gerasimov <karlicoss@gmail.com>
2019-08-28 21:37:03 +0100

    fix extension page handling for chrome

--- tests/firefox_helper.py
+++ tests/firefox_helper.py
@@ -1,11 +1,60 @@
 from pathlib import Path
 import re
+import json
 from time import sleep
+
+
+# TODO can extract hotkeys from Preferences file too ['extensions'] / commands / 
+# "path": "/tmp/.org.chromium.Chromium.TKhNoq/extension_ceedkmkoeooncekjljapnkkjhldddcid",
+#     "commands": {
+#            "linux:Ctrl+Shift+V": {
+#                "command_name": "show_dots",
+#                "extension": "ceedkmkoeooncekjljapnkkjhldddcid",
+#                "global": false
+#            }
+#        },
 
 
 # copy pasted from grasp
 
+
 def open_extension_page(driver, page: str):
+    ff = {
+        'chrome' : get_extension_page_chrome,
+        'firefox': get_extension_page_firefox,
+    }[driver.name]
+    extension_prefix = ff(driver=driver)
+    driver.get(extension_prefix + '/' + page)
+
+
+def get_extension_page_chrome(driver):
+    chrome_profile = Path(driver.capabilities['chrome']['userDataDir'])
+    prefs_file = chrome_profile / 'Default/Preferences'
+    # there are some default extensions as well (e.g. cloud print)
+    # also oddly enough user install extensions don't have manifest information, so we can't find it by name
+
+    # seems to be quite a bit asynchronous (e.g. up to several seconds), so good to be defensive for a bit
+    addon_id = None
+    for _ in range(30):
+        sleep(0.5)
+        if not prefs_file.exists():
+            continue
+
+        prefs = json.loads(prefs_file.read_text())
+        settings = prefs.get('extensions', {}).get('settings', None)
+        if settings is None:
+            continue
+
+        addon_ids = [k for k, v in settings.items() if Path(v['path']).name == f'extension_{k}']
+        if len(addon_ids) == 0:
+            print('addon_ids is none')
+            continue
+        [addon_id] = addon_ids
+    assert addon_id is not None
+    return f'chrome-extension://{addon_id}'
+
+
+def get_extension_page_firefox(driver):
     moz_profile = Path(driver.capabilities['moz:profile'])
     prefs_file = moz_profile / 'prefs.js'
 
@@ -23,8 +72,6 @@ def open_extension_page(driver, page: str):
             continue
         addon_id = m.group(1)
     assert addon_id is not None
-
-    page = f'moz-extension://{addon_id}/{page}'
-    driver.get(page)
+    return f'moz-extension://{addon_id}'
 
 # TODO could also check for errors
