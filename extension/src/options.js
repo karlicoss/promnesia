747e58c51454eeb70938008ffd7eae282cdf9f3a
Dima Gerasimov <karlicoss@gmail.com>
2019-09-08 17:51:57 +0100

    Use webext-options-sync for settings

--- extension/src/options.js
+++ extension/src/options.js
@@ -1,6 +1,9 @@
 /* @flow */
 import type {Src} from './common';
 
+// $FlowFixMe
+import OptionsSync from 'webext-options-sync';
+
 type SrcMap = {
     [Src]: Src
 };
@@ -18,8 +21,9 @@ export type Options = {
 
 }
 
-// TODO how to export settings?? seriously need some standard package for settings
-function default_options(): Options {
+// TODO allow to export settings
+// https://github.com/fregante/webext-options-sync/issues/23
+function defaultOptions(): Options {
     return {
         host: 'http://localhost:13131',
         token: '',
@@ -56,38 +60,17 @@ function default_options(): Options {
     };
 }
 
-export function get_options(cb: (Options) => void)  {
-    // TODO make it as minimal as possible; rest should be async
-    chrome.storage.local.get(null, res => {
-        const optss = res.options || '{}';
-        const saved_opts = JSON.parse(optss);
-        const opts = {...default_options(), ...saved_opts};
-        cb(opts);
-    });
-}
-
-
-function create_promise<T>(provider: ((T) => void) => void): Promise<T> {
-    return new Promise((resolve, reject) => {
-        try {
-            provider(resolve);
-        } catch (err) {
-            reject(err);
-        }
+function optSync() {
+    return new OptionsSync({
+        defaults: defaultOptions(),
     });
 }
 
 // TODO later rename to just get_options
 export async function get_options_async(): Promise<Options> {
-    return await create_promise(get_options);
+    return await (optSync().getAll());
 }
 
 export async function setOptions(opts: Options) {
-    // ugh. making sure every field is a string is too annoying
-    // just easier to store on big settings payload?
-    // TODO can we even store array in local store???
-    const optss = JSON.stringify(opts);
-    console.log('Saving %s', optss);
-
-    await new Promise(cb => chrome.storage.local.set({'options': optss}, cb));
+    await optSync().set(opts);
 }
