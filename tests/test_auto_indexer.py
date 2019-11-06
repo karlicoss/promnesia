e5ab756732395bd4e54f2d7d424d0434170f3eb0
Dima Gerasimov <karlicoss@gmail.com>
2019-11-06 23:37:23 +0000

    test for json locators and context

--- tests/test_auto_indexer.py
+++ tests/test_auto_indexer.py
@@ -1,22 +1,41 @@
+from itertools import groupby
+
 from promnesia.indexers import auto
 
 from common import tdata
 
+sa2464 = 'https://www.scottaaronson.com/blog/?p=2464'
+
 _JSON_URLS = {
     # TODO FIXME only extract one of them?
     'https://johncarlosbaez.wordpress.com/2016/09/09/struggles-with-the-continuum-part-2/',
-    'https://www.scottaaronson.com/blog/?p=2464',
+    sa2464,
 }
 
 
+def makemap(visits):
+    key = lambda v: v.url
+    def iter():
+        for k, g in groupby(sorted(visits, key=key), key=key):
+            yield k, list(sorted(g))
+    return dict(iter())
+
+
 def test_json():
-    # TODO FIXME check context/locator etc
-    assert set(x.url for x in auto.index(tdata('auto/pocket.json'))) == _JSON_URLS
+    mm = makemap(auto.index(tdata('auto/pocket.json')))
+    assert mm.keys() == _JSON_URLS
+
+    # TODO not sure if they deserve separate visits..
+    [v1, v2] = mm[sa2464]
+    assert v1.context == 'list::yyy::given_url'
+    assert v1.locator.href.startswith('emacs:')
+    assert v1.locator.href.endswith('pocket.json')
+    # TODO line number?
 
 
 def test_auto():
-    res = set(x.url for x in auto.index(tdata('auto')))
+    mm = makemap(auto.index(tdata('auto')))
     assert {
         *_JSON_URLS,
         'https://www.youtube.com/watch?v=rHIkrotSwcc',
-    }.issubset(res)
+    }.issubset(mm.keys())
