bbc8bddb708d734ce724703793a9dd5e58f3a0eb
Dima Gerasimov <karlicoss@gmail.com>
2019-11-07 20:48:43 +0000

    handle empty entities properly

--- src/promnesia/indexers/twitter.py
+++ src/promnesia/indexers/twitter.py
@@ -7,6 +7,12 @@ from ..common import Extraction, get_logger, Visit, Loc, PathIsh, extract_urls
 import my.tweets as tw # type: ignore
 
 
+def get(o, k):
+    if isinstance(o, dict):
+        return o[k]
+    else:
+        return getattr(o, k)
+
 def index(export_path: Optional[PathIsh]=None) -> Iterator[Extraction]:
     tw.configure(export_path=export_path)
 
@@ -15,9 +21,11 @@ def index(export_path: Optional[PathIsh]=None) -> Iterator[Extraction]:
     for t in tw.tweets_all():
         try:
             ets = t.entities
-            # TODO entities shouldn't really be None.. figure it out in twidump, perhaps None is returned by api?
-            assert ets is not None
-            urls = [e['expanded_url'] for e in ets['urls']]
+            if ets is not None:
+                urls = [get(e, 'expanded_url') for e in get(ets, 'urls')]
+            else:
+                # TODO entities shouldn't really be None.. figure it out in twidump, perhaps None is returned by api?
+                ets = []
         except Exception as e:
             yield e
             urls = []
