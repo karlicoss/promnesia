f248a59df4bc11c299e7e702723f6d317abd8dbf
Dima Gerasimov <karlicoss@gmail.com>
2020-02-16 20:47:05 +0000

    start work on vcs indexer

--- src/promnesia/indexers/guess.py
+++ src/promnesia/indexers/guess.py
@@ -1,8 +1,24 @@
 # TODO eh. confusing how guess and auto are different...
-from typing import Iterable
+from typing import Iterable, Any
 
 from ..common import Extraction, PathIsh
 
-from . import auto
 
-index = auto.index # TODO later do smth smarter
+def is_git_repo(p: str) -> bool:
+    if '://github.com/' in p:
+        return True
+    return False
+
+
+def index(path: PathIsh, *args, **kwargs) -> Iterable[Extraction]:
+    ps = str(path)
+    # TODO better url detection
+
+    index_: Any # meh
+    if is_git_repo(ps):
+        from . import vcs
+        index_ = vcs.index
+    else:
+        from . import auto
+        index_ = auto.index
+    return index_(path, *args, **kwargs)
---
+++ src/promnesia/indexers/vcs.py
@@ -0,0 +1,8 @@
+from typing import Iterable
+
+from ..common import Extraction, PathIsh
+
+
+def index(path: PathIsh, *args, **kwargs) -> Iterable[Extraction]:
+    repo = str(path)
+    return []
