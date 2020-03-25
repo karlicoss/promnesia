2c24def48f68bafc89ca27bffef3b485811b4b6a
Dima Gerasimov <karlicoss@gmail.com>
2020-03-25 21:48:35 +0000

    record hotkey presses

--- tests/demos.py
+++ tests/demos.py
@@ -7,10 +7,10 @@ from common import uses_x
 from end2end_test import FF, CH, browsers, _test_helper
 from end2end_test import PYTHON_DOC_URL
 from integration_test import index_urls
-from end2end_test import confirm
+from end2end_test import confirm, trigger_command, Command
 from end2end_test import configure_extension
 
-from record import record
+from record import record, hotkeys
 
 @uses_x
 @browsers(FF, CH)
@@ -45,18 +45,26 @@ def real_db():
 def test_demo_show_dots(tmp_path, browser):
     # TODO wonder if it's possible to mess with settings in local storage? unlikely...
     url = 'https://slatestarcodex.com/'
-    with _test_helper(tmp_path, real_db(), None, browser=browser) as helper, record('demos/show-dots.ogv'):
+    with _test_helper(tmp_path, real_db(), None, browser=browser) as helper, hotkeys(), record('demos/show-dots.ogv'):
+        driver = helper.driver
         # TODO make a method of helper??
         configure_extension(
-            helper.driver,
+            driver,
             host=None, port=None, # TODO meh
             notification=False,
         )
+        # TODO extract 'demo_helper' for now??
         # TODO do it before recording??
-        helper.driver.get('about:blank')
+        driver.get('about:blank')
 
         confirm('continue?')
-        helper.driver.get(url)
+        driver.get(url)
+
+        # TODO move driver inside??
+        trigger_command(driver, Command.SHOW_DOTS)
+
+        confirm('continue?')
+
         # TODO would be nice to actually generate subtitles??
         # TODO not sure how long to wait...
         confirm('continue?')
--- tests/record.py
+++ tests/record.py
@@ -1,15 +1,30 @@
 from contextlib import contextmanager
 from pathlib import Path
+from subprocess import Popen
 from typing import Optional
 
 # TODO decorator that records a video if a certain env var/flag is set (pass a custom name too)
+
+@contextmanager
+def hotkeys():
+    # TODO kill in advance??
+    ctx = Popen([
+        'screenkey',
+        '--no-detach',
+        '--timeout', '2',
+        '--key-mode', 'composed',
+    ])
+    with ctx as p:
+        try:
+            yield p
+        finally:
+            p.kill()
 
 
 @contextmanager
 def record(output: Optional[Path]=None):
     assert output is not None, "TODO use tmp file or current dir??"
 
-    from subprocess import Popen
     # TODO window-id
     ctx = Popen([
         'recordmydesktop',
@@ -17,6 +32,8 @@ def record(output: Optional[Path]=None):
         '--v_quality=1', # TODO fix quality later
         '--on-the-fly-encoding',
         '--workdir=/tmp', # TODO not sure..
+
+        '--overwrite', # TODO make optional?
         '--output', output,
     ])
     with ctx as p:
