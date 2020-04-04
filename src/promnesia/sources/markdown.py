499e8ca00b5954dcc57cc3d861637f4e731d6ce4
Dima Gerasimov <karlicoss@gmail.com>
2020-04-04 13:06:01 +0100

    extract markdown contexts properly (for now render as HTML)

--- src/promnesia/sources/markdown.py
+++ src/promnesia/sources/markdown.py
@@ -8,6 +8,14 @@ from ..common import get_logger, Extraction, Url, PathIsh, Res, Visit, echain, L
 import mistletoe # type: ignore
 from mistletoe import Document # type: ignore
 from mistletoe.span_token import AutoLink, Link # type: ignore
+import mistletoe.block_token as BT # type: ignore
+from mistletoe.html_renderer import HTMLRenderer # type: ignore
+
+
+renderer = HTMLRenderer()
+
+
+block_tokens = tuple(getattr(BT, name) for name in BT.__all__)
 
 
 class Parsed(NamedTuple):
@@ -18,29 +26,52 @@ class Parsed(NamedTuple):
 Result = Res[Parsed]
 
 
-def _extract(cur) -> Iterator[Parsed]:
-    # TODO take in surrounding context??
-    if not isinstance(cur, (AutoLink, Link)):
-        # hopefully that's all??
-        return
+# the fuck...
+#
+# from mistletoe import Document
+# d = Document('''
+# # heading
+# ## sub
+# ## sub2
+# ''')
+# d.children[0].content
+# Out[13]: 'sub2'
 
-    url = cur.target
-    if type(url) != str:
-        print(url, type(url))
-    context = None
-    yield Parsed(url=url, context=context)
+
+class Parser:
+    def __init__(self, path: Path):
+        self.doc = mistletoe.Document(path.read_text())
+
+    def _extract(self, cur, last_block) -> Iterator[Parsed]:
+        if not isinstance(cur, (AutoLink, Link)):
+            # hopefully that's all??
+            return
+
+        url = cur.target
+        # TODO fuck. it doesn't preserve line numbers/positions in text???
+
+        # ugh. It can't output markdown.. https://github.com/miyuchina/mistletoe/issues/4
+        context = None if last_block is None else renderer.render(last_block)
+        yield Parsed(url=url, context=context)
 
 
-def _walk(cur: mistletoe.Document) -> Iterator[Result]:
-    try:
-        yield from _extract(cur)
-    except Exception as e:
-        # TODO log context??
-        yield e
+    def _walk(self, cur, last_block) -> Iterator[Result]:
+        if isinstance(cur, block_tokens):
+            last_block = cur
 
-    children = getattr(cur, 'children', [])
-    for c in children:
-        yield from _walk(c)
+        try:
+            yield from self._extract(cur, last_block)
+        except Exception as e:
+            # TODO log context??
+            yield e
+
+        children = getattr(cur, 'children', [])
+        for c in children:
+            yield from self._walk(c, last_block=last_block)
+
+
+    def walk(self):
+        yield from self._walk(self.doc, last_block=None)
 
 
 def extract_from_file(fname: PathIsh) -> Iterator[Extraction]:
@@ -49,8 +80,8 @@ def extract_from_file(fname: PathIsh) -> Iterator[Extraction]:
 
     ex = RuntimeError(f'while extracting from {path}')
 
-    doc = mistletoe.Document(path.read_text())
-    for r in _walk(doc):
+    p = Parser(path)
+    for r in p.walk():
         if isinstance(r, Exception):
             yield echain(ex, r)
         else:
