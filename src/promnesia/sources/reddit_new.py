8888aca17e8c6b0663f7471cd5164d5342b848f5
Dima Gerasimov <karlicoss@gmail.com>
2020-05-06 20:10:26 +0100

    extract comments, submissions and upvotes from reddit

--- src/promnesia/sources/reddit_new.py
+++ src/promnesia/sources/reddit_new.py
@@ -4,7 +4,7 @@ from typing import Iterable
 from ..common import Visit, Loc, extract_urls, Extraction
 
 
-from my.reddit import Save
+from my.reddit import Submission, Comment, Save, Upvote
 from my.reddit import submissions, comments, saved, upvoted
 
 
@@ -16,21 +16,82 @@ def index() -> Iterable[Extraction]:
             yield from _from_save(s)
         except Exception as e:
             yield e
-    # TODO FIXME process the rest
+
+    for c in comments():
+        try:
+            yield from _from_comment(c)
+        except Exception as e:
+            yield e
+
+    for sub in submissions():
+        try:
+            yield from _from_submission(sub)
+        except Exception as e:
+            yield e
+
+    for u in upvoted():
+        try:
+            yield from _from_upvote(u)
+        except Exception as e:
+            yield e
 
 
-def _from_save(s: Save) -> Iterable[Extraction]:
+# TODO not sure if worthy a Protocol?? duplication is a bit annoying..
+
+def _from_save(i: Save) -> Iterable[Extraction]:
     locator = Loc.make(
-        title='Reddit save',
-        href=s.url,
+        title='saved',
+        href=i.url,
     )
     # TODO FIXME should probably use markdown link parser throughout the module
     # TODO worth having the link that was submitted?
     # it's different from the reddit link
-    for url in chain([s.url], extract_urls(s.text)):
+    for url in chain([i.url], extract_urls(i.text)):
         yield Visit(
             url=url,
-            dt=s.created,
-            context=s.text,
+            dt=i.created,
+            context=i.text,
+            locator=locator,
+        )
+
+
+def _from_comment(i: Comment) -> Iterable[Extraction]:
+    locator = Loc.make(
+        title='comment',
+        href=i.url,
+    )
+    for url in chain([i.url], extract_urls(i.text)):
+        yield Visit(
+            url=url,
+            dt=i.created,
+            context=i.text,
+            locator=locator,
+        )
+
+
+def _from_submission(i: Submission) -> Iterable[Extraction]:
+    locator = Loc.make(
+        title=f'submission: {i.title}',
+        href=i.url,
+    )
+    for url in chain([i.url], extract_urls(i.text)):
+        yield Visit(
+            url=url,
+            dt=i.created,
+            context=i.text,
+            locator=locator,
+        )
+
+
+def _from_upvote(i: Submission) -> Iterable[Extraction]:
+    locator = Loc.make(
+        title=f'upvoted',
+        href=i.url,
+    )
+    for url in chain([i.url], extract_urls(i.text)):
+        yield Visit(
+            url=url,
+            dt=i.created,
+            context=i.text,
             locator=locator,
         )
