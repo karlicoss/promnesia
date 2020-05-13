75f6ddff55a930254c7d5cb56149588ad6cf3a1c
Dima Gerasimov <karlicoss@gmail.com>
2020-05-13 22:37:11 +0100

    add my.rss source

---
+++ src/promnesia/sources/rss.py
@@ -0,0 +1,25 @@
+from itertools import chain
+from typing import Iterable
+
+from ..common import Visit, Loc, extract_urls, Extraction, get_logger
+
+from datetime import datetime
+
+import pytz
+
+# arbitrary,  2011-11-04 00:05:23.283+00:00
+default_datetime = datetime.fromtimestamp(1320365123, tz=pytz.utc)
+# TODO FIXME allow for visit not to have datetime?
+# I.e. even having context is pretty good!
+
+def index() -> Iterable[Extraction]:
+    from my.rss.all import subscriptions
+    for feed in subscriptions():
+        # TODO locator should be optional too? although could use direct link in the rss reader interface
+        locator = Loc.make(title='my.rss')
+        yield Visit(
+            url=feed.url,
+            dt=feed.created_at or default_datetime,
+            context=f'RSS subscription', # TODO use 'provider', etc?
+            locator=locator,
+        )
