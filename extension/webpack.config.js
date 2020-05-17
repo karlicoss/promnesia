5a6db2c0b929deecf09885a5ed426701f01258eb
Dima Gerasimov <karlicoss@gmail.com>
2020-05-17 10:16:58 +0100

    extension: fix search page on mobile, remove unnecessary popup page

--- extension/webpack.config.js
+++ extension/webpack.config.js
@@ -69,11 +69,9 @@ const action = {
 };
 
 
-// TODO not sure why it is here?
+// NOTE: ok, this is important on Android, this is where click in the menu goes
 if (isMobile) {
-    action["default_popup"] = "popup.html";
-    // TODO ok, need to refine and add things on that page...
-    // TODO maybe show visits as on sidebar?
+    action["default_popup"] = "search.html";
 }
 
 const permissionsExtra = [];
@@ -160,8 +158,6 @@ const options = {
   entry: {
     background   : path.join(__dirname, './src/background'),
     options_page : path.join(__dirname, './src/options_page'),
-      // TODO remove popup?
-    popup        : path.join(__dirname, './src/popup'),
     sidebar      : path.join(__dirname, './src/sidebar'),
     search       : path.join(__dirname, './src/search'),
     background_injector : path.join(__dirname, './src/background_injector'),
