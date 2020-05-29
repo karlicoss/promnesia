5c4e6a29e8eb707a351b07ed2d0816a98fbe12be
Dima Gerasimov <karlicoss@gmail.com>
2020-05-29 23:15:26 +0100

    docs: add 'data flow'

--- src/promnesia/sources/pocket.py
+++ src/promnesia/sources/pocket.py
@@ -10,19 +10,20 @@ def index() -> Results:
 
     for a in get_articles():
         # TODO not sure if can make more specific link on pocket?
-        loc = Loc.make(title='pocket', href=a.pocket_link)
-        hls = a.highlights
+        loc = Loc.make(title='pocket', href=a.pocket_link)  # type: ignore[attr-defined]
+        hls = a.highlights  # type: ignore[attr-defined]
         if len(hls) == 0:
             yield Visit(
-                url=a.url,
-                dt=a.added,
+                url=a.url, # type: ignore[attr-defined]
+                dt=a.added, # type: ignore[attr-defined]
                 context=None,
                 locator=loc,
             )
         for hl in hls:
             yield Visit(
-                url=a.url,
+                url=a.url, # type: ignore[attr-defined]
                 dt=hl.created,
                 context=hl.text,
                 locator=loc,
             )
+# TODO to properly typecheck, add pockexport, hypexport etc., cloning to lint script
