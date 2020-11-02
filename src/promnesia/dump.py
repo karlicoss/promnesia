90416a3093614c7d2c28ec16666a06edfc451141
Dima Gerasimov <karlicoss@gmail.com>
2020-11-02 01:46:13 +0000

    general: experimental support for updating the index rather than indexing from scratch
    for now controlled via `PROMNESIA_INDEX_POLICY=update` env variable, but later will be properly added to the cmdline

--- src/promnesia/dump.py
+++ src/promnesia/dump.py
@@ -1,6 +1,7 @@
+import os
 from pathlib import Path
-from typing import Dict, List, Tuple
 import shutil
+from typing import Dict, List, Tuple, Set
 
 from more_itertools import chunked
 
@@ -12,6 +13,12 @@ from cachew import NTBinder
 from .common import get_logger, DbVisit, get_tmpdir
 from . import config
 
+
+# NOTE: experimental.. need to make it a proper cmdline argument later
+INDEX_POLICY = os.environ.get('PROMNESIA_INDEX_POLICY', 'overwrite_all')
+# if 'update' is passed, will run against the existing db and only tough the sources present in the current index run
+# not sue if a good name for this..
+policy_update = INDEX_POLICY == 'update'
 
 # NOTE: I guess the main performance benefit from this is not creating too many tmp lists and avoiding overhead
 # since as far as sql is concerned it should all be in the same transaction. only a guess
@@ -34,19 +41,31 @@ def dump_histories(all_histories: List[Tuple[str, List[DbVisit]]]) -> None:
             yield from h
 
     tpath = Path(get_tmpdir().name) / 'promnesia.tmp.sqlite'
-    engine = create_engine(f'sqlite:///{tpath}')
+    if not policy_update:
+        engine = create_engine(f'sqlite:///{tpath}')
+    else:
+        engine = create_engine(f'sqlite:///{db_path}')
+
     binder = NTBinder.make(DbVisit)
     meta = MetaData(engine)
     table = Table('visits', meta, *binder.columns)
     meta.create_all()
 
+    cleared: Set[str] = set()
     with engine.begin() as conn:
         for chunk in chunked(iter_visits(), n=_CHUNK_BY):
+            srcs = set(v.src for v in chunk)
+            new = srcs.difference(cleared)
+            for src in new:
+                conn.execute(table.delete().where(table.c.src == src))
+                cleared.add(src)
+
             bound = [binder.to_row(x) for x in chunk]
             # pylint: disable=no-value-for-parameter
             conn.execute(table.insert().values(bound))
 
-    shutil.move(str(tpath), str(db_path))
+    if not policy_update:
+        shutil.move(str(tpath), str(db_path))
 
     logger.info('saved database to %s', db_path)
     # TODO log error count
