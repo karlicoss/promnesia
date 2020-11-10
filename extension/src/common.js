89907b44297b4bba5694febbadf86c3e9c5a34a3
Dima Gerasimov <karlicoss@gmail.com>
2020-11-10 00:34:18 +0000

    extension: use message passing instead of explicit jsonify

--- extension/src/common.js
+++ extension/src/common.js
@@ -116,6 +116,7 @@ export class Blacklisted {
 
 export const Methods = {
     GET_SIDEBAR_VISITS  : 'getActiveTabVisitsForSidebar',
+    BIND_SIDEBAR_VISITS : 'bindSidebarVisits',
     SEARCH_VISITS_AROUND: 'searchVisitsAround',
     MARK_VISITED        : 'markVisited',
     OPEN_SEARCH         : 'openSearch',
