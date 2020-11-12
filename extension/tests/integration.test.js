77a47fb9e5539c8b200a605c109718972bab525a
Dima Gerasimov <karlicoss@gmail.com>
2020-11-12 23:18:37 +0000

    extension: add initial integration tests

---
+++ extension/tests/integration.test.js
@@ -0,0 +1,37 @@
+import {setOptions, getOptions} from '../src/options'
+
+global.chrome = {
+    storage: {
+        sync: {
+            // meh.
+            get: function (name, res) {
+                res({'options': {
+                    host: 'http://badhost:43210', // some random port
+                }})
+            }
+        },
+    },
+    runtime: {
+        lastError: null,
+    }
+}
+
+
+test('options', async () => {
+    // shouldn't crash at least..
+    const opts = await getOptions()
+})
+// TODO could check options migrations?
+
+import fetch from 'node-fetch'
+global.fetch = fetch
+
+
+import {getBackendVisits} from '../src/api'
+test('visits', async() => {
+    // const opts = await getOptions()
+    // opts.host = host: 'http//bad.host',
+    // TODO have a defensive and offensive modes?
+    // but for network errors makes sense...
+    const vis = await getBackendVisits()
+})
