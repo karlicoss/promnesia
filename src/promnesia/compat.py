0703ffad16a56e002113ef5404775d83d61d9e98
Kostis Anagnostopoulos <ankostis@gmail.com>
2021-03-06 21:57:52 +0200

    fix(main) argparse 'extend" action on <py38

--- src/promnesia/compat.py
+++ src/promnesia/compat.py
@@ -11,6 +11,25 @@ def _fix(args: Paths) -> List[str]:
     assert not isinstance(args, str), args # just to prevent shell=True calls...
     return list(map(str, args))
 
+
+import argparse
+
+def register_argparse_extend_action_in_pre_py38(parser: argparse.ArgumentParser):
+    import sys
+
+    if sys.version_info < (3, 8):
+
+        class ExtendAction(argparse.Action):
+
+            def __call__(self, parser, namespace, values, option_string=None):
+                items = getattr(namespace, self.dest) or []
+                items.extend(values)
+                setattr(namespace, self.dest, items)
+
+
+        parser.register('action', 'extend', ExtendAction)
+
+
 import subprocess
 
 def run(args: Paths, **kwargs) -> subprocess.CompletedProcess:
@@ -18,10 +37,10 @@ def run(args: Paths, **kwargs) -> subprocess.CompletedProcess:
 
 def check_call(args: Paths, **kwargs) -> None:
     subprocess.check_call(_fix(args), **kwargs)
-    
+
 def check_output(args: Paths, **kwargs) -> bytes:
     return subprocess.check_output(_fix(args), **kwargs)
-    
+
 def Popen(args: Paths, **kwargs) -> subprocess.Popen:
     return subprocess.Popen(_fix(args), **kwargs)
 
