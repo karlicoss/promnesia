06a69adf11dbe4ee09c95a58896367f46ac23dd9
Dima Gerasimov <karlicoss@gmail.com>
2022-04-24 15:52:51 +0100

    fix test condition

--- src/promnesia/sources/logseq.py
+++ src/promnesia/sources/logseq.py
@@ -1,7 +1,7 @@
 import os.path
 
 def logseq_replacer(path: str, root: str) -> str:
-    if not path.startswith("editor://") or not path.endswith('.md') or not path.endswith('.org'):
+    if not path.startswith("editor://") or not (path.endswith('.md') or path.endswith('.org')):
         return path
         
     graph = os.path.basename(root)
