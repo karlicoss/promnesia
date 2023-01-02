1d289e65aedff6b0b0affebd18f74e8d1daab98a
Dima Gerasimov <karlicoss@gmail.com>
2023-01-02 16:50:17 +0000

    end2end test: cleanup test_sidebar_basic, run on all browsers
    + extract iframe context manager

---
+++ tests/webdriver_utils.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+from contextlib import contextmanager
+from typing import Optional, Iterator
+
+
+from selenium.webdriver import Remote as Driver
+from selenium.webdriver.remote.webelement import WebElement
+
+
+def get_current_frame(driver: Driver) -> Optional[WebElement]:
+    # idk why is it so hard to get current frame in selenium, but it is what it is
+    # https://github.com/seleniumhq/selenium-google-code-issue-archive/issues/4305#issuecomment-192026569
+    return driver.execute_script('return window.frameElement')
+
+
+@contextmanager
+def frame_context(driver: Driver, frame) -> Iterator[Optional[WebElement]]:
+    # todo return the frame maybe?
+    current = get_current_frame(driver)
+    driver.switch_to.frame(frame)
+    try:
+        new_frame = get_current_frame(driver)
+        yield new_frame
+    finally:
+        driver.switch_to.frame(current)
