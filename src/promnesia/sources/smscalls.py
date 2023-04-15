f8525e762603c208d49bc7f3f30c880c668f25b7
Sean Breckenridge <seanbrecke@gmail.com>
2023-04-14 23:04:01 -0700

    sources.smscalls: propogate errors if there are breaking schema changes

--- src/promnesia/sources/smscalls.py
+++ src/promnesia/sources/smscalls.py
@@ -11,11 +11,18 @@ def index() -> Results:
 
     for m in messages():
 
+        if isinstance(m, Exception):
+            yield m
+            continue
+
         urls = extract_urls(m.message)
         if len(urls) == 0:
             continue
 
-        loc = Loc(title=f"SMS with {m.who} ({m.phone_number})")
+        if m.who is None:
+            loc = Loc(title=f"SMS with {m.phone_number}")
+        else:
+            loc = Loc(title=f"SMS with {m.who} ({m.phone_number})")
 
         for u in urls:
             yield Visit(
