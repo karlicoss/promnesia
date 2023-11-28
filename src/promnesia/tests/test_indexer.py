62839c72363bf39455cf33937e7efc81c0bdf5c1
Dima Gerasimov <karlicoss@gmail.com>
2023-11-28 01:37:55 +0000

    move concurrent indexer test to src

--- src/promnesia/tests/test_indexer.py
+++ src/promnesia/tests/test_indexer.py
@@ -1,10 +1,12 @@
 from collections import Counter
 import inspect
 from pathlib import Path
+from subprocess import check_call, Popen
 from textwrap import dedent
 
 import pytest
 
+from .common import promnesia_bin
 from ..__main__ import do_index
 from ..database.load import get_all_db_visits
 
@@ -14,22 +16,19 @@ def get_stats(tmp_path: Path) -> Counter:
     return Counter(v.src for v in visits)
 
 
+def write_config(path: Path, gen) -> None:
+    output_dir = path.parent
+    cfg_src = dedent('\n'.join(inspect.getsource(gen).splitlines()[1:])) + f"\nOUTPUT_DIR = r'{output_dir}'"
+    path.write_text(cfg_src)
+
+
 @pytest.mark.parametrize('mode', ['update', 'overwrite'])
 def test_indexing_mode(tmp_path: Path, mode: str) -> None:
     # ugh. we modify the config very fast during tests
     # and pycache distinguishes identical filenames based on int mtime in seconds
     # so best to use different names to prevent undesired caching
     # https://github.com/python/cpython/blob/fb202af4470d6051a69bb9d2f44d7e8a1c99eb4f/Lib/importlib/_bootstrap_external.py#L714-L739
-    # TODO could probably get rid of that if we switch from importlib config loading to exec()?
-    config_count = 0
-
-    def write_config(output: Path, gen) -> Path:
-        nonlocal config_count
-        cfg_path = output / f'config{config_count}.py'
-        config_count += 1
-        cfg_src = dedent('\n'.join(inspect.getsource(gen).splitlines()[1:])) + f"\nOUTPUT_DIR = r'{output}'"
-        cfg_path.write_text(cfg_src)
-        return cfg_path
+    # TODO could probably relax that if we switch from importlib config loading to exec()?
 
     def cfg1() -> None:
         from promnesia.common import Source
@@ -40,8 +39,9 @@ def test_indexing_mode(tmp_path: Path, mode: str) -> None:
             Source(demo.index, count=20, base_dt='2001-01-01', delta=30, name='demo2'),
         ]
 
-    cfg = write_config(tmp_path, cfg1)
-    do_index(cfg)
+    cfg_path = tmp_path / 'config1.py'
+    write_config(cfg_path, cfg1)
+    do_index(cfg_path)
 
     stats = get_stats(tmp_path)
     assert stats == {'demo1': 10, 'demo2': 20}
@@ -55,8 +55,9 @@ def test_indexing_mode(tmp_path: Path, mode: str) -> None:
             Source(demo.index, count=40, base_dt='2010-01-01', delta=30, name='demo3'),
         ]
 
-    cfg = write_config(tmp_path, cfg2)
-    do_index(cfg, overwrite_db={'overwrite': True, 'update': False}[mode])
+    cfg_path = tmp_path / 'config2.py'
+    write_config(cfg_path, cfg2)
+    do_index(cfg_path, overwrite_db={'overwrite': True, 'update': False}[mode])
     # TODO use some sort of test helper?
     stats = get_stats(tmp_path)
 
@@ -66,3 +67,47 @@ def test_indexing_mode(tmp_path: Path, mode: str) -> None:
     else:
         # should overwrite with newly indexed visits
         assert stats == {'demo2': 30, 'demo3': 40}
+
+
+# TODO check both modes?
+def test_concurrent_indexing(tmp_path: Path) -> None:
+    def cfg_fast() -> None:
+        from promnesia.common import Source
+        from promnesia.sources import demo
+
+        SOURCES = [Source(demo.index, count=10)]
+
+    cfg_fast_path = tmp_path / 'cfg_fast.py'
+    write_config(cfg_fast_path, cfg_fast)
+
+    def cfg_slow() -> None:
+        from promnesia.common import Source
+        from promnesia.sources import demo
+
+        SOURCES = [Source(demo.index, count=100_000)]
+
+    cfg_slow_path = tmp_path / 'cfg_slow.py'
+    write_config(cfg_slow_path, cfg_slow)
+
+    # init it first, to create the database
+    # TODO ideally this shouldn't be necessary but it's reasonable that people would already have the index
+    # otherwise it would fail at db creation point.. which is kinda annoying to work around
+    # todo in principle can work around same way as in cachew, by having a loop around PRAGMA WAL command?
+    check_call(promnesia_bin('index', '--config', cfg_fast_path, '--overwrite'))
+
+    total_runs = 0
+    # run in the background
+    with Popen(promnesia_bin('index', '--config', cfg_slow_path, '--overwrite')) as slow_indexer:
+        while slow_indexer.poll() is None:
+            # create a bunch of 'smaller' indexers running in parallel
+            fasts = [
+                Popen(promnesia_bin('index', '--config', cfg_fast_path, '--overwrite'))
+                for _ in range(10)
+            ]
+            for fast in fasts:
+                assert fast.wait() == 0, fast  # should succeed
+                total_runs += 1
+        assert slow_indexer.poll() == 0, slow_indexer
+
+    # if not, slow indexer is too fast, so crank up the count in it
+    assert total_runs > 20
--- tests/integration_test.py
+++ tests/integration_test.py
@@ -1,12 +1,11 @@
 from datetime import datetime, timedelta
 from pathlib import Path
-from subprocess import check_call, Popen
 from textwrap import dedent
 from typing import  Optional, Union, Sequence, Tuple, Mapping
 
 import pytest
 
-from common import DATA, promnesia_bin
+from common import DATA
 
 from promnesia.common import _is_windows
 from promnesia.database.load import get_all_db_visits
@@ -149,6 +148,7 @@ SOURCES = [chrome_extractor]
     run_index(cfg)
 
 
+# TODO this should be in hypothesis source tester?
 def test_hypothesis(tmp_path: Path) -> None:
     index_hypothesis(tmp_path)
     visits = get_all_db_visits(tmp_path / 'promnesia.sqlite')
@@ -244,31 +244,3 @@ OUTPUT_DIR = r'{tmp_path}'
 
     [e, _, _, _] = visits
     assert e.src == 'error'
-
-
-@pytest.mark.parametrize('execution_number', range(1))  # adjust this parameter to increase 'coverage
-def test_concurrent_indexing(tmp_path: Path, execution_number) -> None:
-    cfg_slow = tmp_path / 'config_slow.py'
-    cfg_fast = tmp_path / 'config_fast.py'
-    cfg = dedent(f'''
-    OUTPUT_DIR = r'{tmp_path}'
-    from promnesia.common import Source
-    from promnesia.sources import demo
-    SOURCES = [Source(demo.index, count=COUNT)]
-    ''')
-    cfg_slow.write_text(cfg.replace('COUNT', '100000'))
-    cfg_fast.write_text(cfg.replace('COUNT', '100'   ))
-    # init it first, to create the database
-    # TODO ideally this shouldn't be necessary but it's reasonable that people would already have the index
-    # otherwise it would fail at db creation point.. which is kinda annoying to work around
-    # todo in principle can work around same way as in cachew, by having a loop around PRAGMA WAL command?
-    check_call(promnesia_bin('index', '--config', cfg_fast))
-
-    # run in the background
-    with Popen(promnesia_bin('index', '--config', cfg_slow)) as slow:
-        while slow.poll() is None:
-            # create a bunch of 'smaller' indexers running in parallel
-            fasts = [Popen(promnesia_bin('index', '--config', cfg_fast)) for _ in range(10)]
-            for fast in fasts:
-                assert fast.wait() == 0, fast  # should succeed
-        assert slow.poll() == 0, slow
