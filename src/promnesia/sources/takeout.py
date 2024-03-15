01d0f041f420a907a011d907f1cc7b18422b8fc7
Sean Breckenridge <seanbrecke@gmail.com>
2024-03-15 14:56:28 -0700

    defensive csv youtube imports

--- src/promnesia/sources/takeout.py
+++ src/promnesia/sources/takeout.py
@@ -1,11 +1,16 @@
 '''
 Uses HPI [[https://github.com/karlicoss/HPI/blob/master/doc/MODULES.org#mygoogletakeoutpaths][google.takeout]] module
 '''
-from typing import Iterable, Set, Any
+from typing import Iterable, Set, Any, NamedTuple
 import warnings
 
 from ..common import Visit, Loc, Results, logger
 from ..compat import removeprefix
+
+
+# incase user is using an old version of google_takeout_parser
+class YoutubeCSVStub(NamedTuple):
+    contentJSON: str
 
 
 def index() -> Results:
@@ -14,7 +19,7 @@ def index() -> Results:
 
     try:
         from my.google.takeout.parser import events
-        from google_takeout_parser.models import Activity, YoutubeComment, LikedYoutubeVideo, ChromeHistory, CSVYoutubeComment, CSVYoutubeLiveChat
+        from google_takeout_parser.models import Activity, YoutubeComment, LikedYoutubeVideo, ChromeHistory
         from google_takeout_parser.parse_csv import reconstruct_comment_content, extract_comment_links
     except ModuleNotFoundError as ex:
         logger.exception(ex)
@@ -26,12 +31,24 @@ def index() -> Results:
         yield from takeout_legacy.index()
         return
 
+
     _seen: Set[str] = {
         # these are definitely not useful for promnesia
         'Location',
         'PlaceVisit',
         'PlayStoreAppInstall',
     }
+
+    imported_yt_csv_models = False
+    try:
+        from google_takeout_parser.models import CSVYoutubeComment, CSVYoutubeLiveChat
+        imported_yt_csv_models = True
+    except ImportError:
+        # warn user to upgrade google_takeout_parser
+        warnings.warn("Please upgrade google_takeout_parser (`pip install -U google_takeout_parser`) to support the new format for youtube comments")
+        CSVYoutubeComment = YoutubeCSVStub
+        CSVYoutubeLiveChat = YoutubeCSVStub
+
     def warn_once_if_not_seen(e: Any) -> Iterable[Exception]:
         et_name = type(e).__name__
         if et_name in _seen:
@@ -111,7 +128,7 @@ def index() -> Results:
                 yield Visit(
                     url=url, dt=e.dt, context=e.content, locator=Loc(title=e.content, href=url)
                 )
-        elif isinstance(e, CSVYoutubeComment):
+        elif imported_yt_csv_models and isinstance(e, CSVYoutubeComment):
             contentJSON = json.loads(e.contentJSON)
             content = reconstruct_comment_content(contentJSON, format='text')
             if isinstance(content, Exception):
@@ -129,7 +146,7 @@ def index() -> Results:
             yield Visit(
                 url=e.video_url, dt=e.dt, context=content, locator=Loc(title=context, href=e.video_url)
             )
-        elif isinstance(e, CSVYoutubeLiveChat):
+        elif imported_yt_csv_models and isinstance(e, CSVYoutubeLiveChat):
             contentJSON = json.loads(e.contentJSON)
             content = reconstruct_comment_content(contentJSON, format='text')
             if isinstance(content, Exception):
