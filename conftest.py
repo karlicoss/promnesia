da6df37cc99638357c1f2aacf502e19402dc5e58
Dima Gerasimov <karlicoss@gmail.com>
2025-06-24 00:35:46 +0100

    general: bring conftest/pytest.ini/tox.ini up to date

--- conftest.py
+++ conftest.py
@@ -20,6 +20,8 @@ namespace_pkg_dirs = [str(d) for d in root_dir.iterdir() if d.is_dir()]
 # resolve_package_path is called from _pytest.pathlib.import_path
 # takes a full abs path to the test file and needs to return the path to the 'root' package on the filesystem
 resolve_pkg_path_orig = _pytest.pathlib.resolve_package_path
+
+
 def resolve_package_path(path: pathlib.Path) -> Optional[pathlib.Path]:
     result = path  # search from the test file upwards
     for parent in result.parents:
@@ -30,7 +32,12 @@ def resolve_package_path(path: pathlib.Path) -> Optional[pathlib.Path]:
         if path.name == 'conftest.py':
             return resolve_pkg_path_orig(path)
     raise RuntimeError("Couldn't determine path for ", path)
-_pytest.pathlib.resolve_package_path = resolve_package_path
+
+
+# NOTE: seems like it's not necessary anymore?
+# keeping it for now just in case
+# after https://github.com/pytest-dev/pytest/pull/13426 we should be able to remove the whole conftest
+# _pytest.pathlib.resolve_package_path = resolve_package_path
 
 
 # without patching, the orig function returns just a package name for some reason
@@ -38,10 +45,14 @@ _pytest.pathlib.resolve_package_path = resolve_package_path
 # so we need to point it at the absolute path properly
 # not sure what are the consequences.. maybe it wouldn't be able to run against installed packages? not sure..
 search_pypath_orig = _pytest.main.search_pypath
+
+
 def search_pypath(module_name: str) -> str:
     mpath = root_dir / module_name.replace('.', os.sep)
     if not mpath.is_dir():
         mpath = mpath.with_suffix('.py')
         assert mpath.exists(), mpath  # just in case
     return str(mpath)
+
+
 _pytest.main.search_pypath = search_pypath
